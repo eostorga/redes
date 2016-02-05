c56bd5ef1e4de5552bd26cf5cbf6839ff17bcafe
erick-ostorga <ostorgach.erick@gmail.com>
2016-02-05 13:44:03 -0600

    Divide archivo en un arreglo
    Tiene un poco del algoritmo para enviar solo la ventana.

--- Cliente.py
+++ Cliente.py
@@ -1,44 +1,51 @@
 # -*- coding: utf-8 -*-
-
 # Programa Cliente
+# Argumentos en la linea de comandos
+# python [0: cliente.py] [1: modo] [2: archivo] [3: puerto] [4: ventana] [5: timeout]
+
 import socket
 import sys
+import time
  
 # Creando el socket TCP/IP para conectarse al intermediario
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-'''
-Argumentos en la linea de comandos
-python [0: cliente.py] [1: modo] [2: archivo] [3: puerto] [4: ventana] [5: timeout]
-'''
+
 # Obtiene el numero de puerto donde escucha el intermediario
 port_number = int(sys.argv[3])
-
 # Conecta el socket en el puerto cuando el intermediario este escuchando
 interm_address = ('localhost', port_number)
-print >>sys.stderr, 'Conectando a %s en el puerto %s' % interm_address
 client_sock.connect(interm_address)
 
-try:     
-    # Enviando datos
+def rdt_send(message_array):
+    global next_seq_num
+    # Envia solo la ventana 
+    while next_seq_num < base_seq_num + window_size:
+        packet = str(next_seq_num)+':'+message_array[next_seq_num]
+        client_sock.send(packet)
+        next_seq_num += 1
+
+def open_file():
     with open(sys.argv[2],'r') as file_to_send:
         message = file_to_send.read()
     file_to_send.close()
-
     # Separa la string en un arreglo de caracteres y envia uno a uno al servidor
-    message_array = list(message)
-    for chart in range(0, len(message_array)):
-        client_sock.sendall(str(chart)+':'+message_array[chart]+'\n')
- 
-    # Buscando respuesta
-    amount_received = 0
-    amount_expected = len(message)
-     
-    while amount_received < amount_expected:
-        data = client_sock.recv(19)
-        amount_received += len(data)
-        print >>sys.stderr, 'Recibiendo "%s"' % data
- 
+    return list(message);
+
+try:
+    message_array = open_file()
+    window_size = int(sys.argv[4])
+    time_to_wait = int(sys.argv[5])
+    # Window size <= sequence range/2
+    seq_range = (window_size*2) # range = [0,seq_range]
+    base_seq_num = 0
+    next_seq_num = 0
+    
+    # Enviando datos 
+    rdt_send(message_array)
+
+    data = client_sock.recv(19)
+    print >>sys.stderr, 'Recibiendo %s' % data
+
 finally:
     print >>sys.stderr, 'Cerrando socket'
     client_sock.close()
