71797000d81432f490f8803e973fe6e8fdacc9a1
erick-ostorga <ostorgach.erick@gmail.com>
2016-02-06 15:08:03 -0600

    Manda el paquete con el número de secuencia correcto

--- Cliente.py
+++ Cliente.py
@@ -9,7 +9,6 @@ import time
  
 # Creando el socket TCP/IP para conectarse al intermediario
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # Obtiene el numero de puerto donde escucha el intermediario
 port_number = int(sys.argv[3])
 # Conecta el socket en el puerto cuando el intermediario este escuchando
@@ -17,12 +16,16 @@ interm_address = ('localhost', port_number)
 client_sock.connect(interm_address)
 
 def rdt_send(message_array):
-    global next_seq_num
+    global next_seq_num, count
     # Envia solo la ventana 
     while next_seq_num < base_seq_num + window_size:
         packet = str(next_seq_num)+':'+message_array[next_seq_num]
-        client_sock.send(packet)
-        next_seq_num += 1
+        client_sock.sendall(packet)
+        next_seq_num = count % seq_range # Asigna el siguiente numero de secuencia
+        count += 1
+        time.sleep(0.1)
+
+def rdt_recv():
 
 def open_file():
     with open(sys.argv[2],'r') as file_to_send:
@@ -39,11 +42,12 @@ try:
     seq_range = (window_size*2) # range = [0,seq_range]
     base_seq_num = 0
     next_seq_num = 0
-    
+    count = 1
+
     # Enviando datos 
     rdt_send(message_array)
 
-    data = client_sock.recv(19)
+    data = client_sock.recv(1000)
     print >>sys.stderr, 'Recibiendo %s' % data
 
 finally:
