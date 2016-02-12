3ef1481617b9e8059101692f5678fc0aca32c95f
erick-ostorga <ostorgach.erick@gmail.com>
2016-02-12 10:03:13 -0600

    Ciclo donde envía y espera recibir ACK

--- Cliente.py
+++ Cliente.py
@@ -6,7 +6,7 @@
 import socket
 import sys
 import time
- 
+
 # Creando el socket TCP/IP para conectarse al intermediario
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Obtiene el numero de puerto donde escucha el intermediario
@@ -14,18 +14,34 @@ port_number = int(sys.argv[3])
 # Conecta el socket en el puerto cuando el intermediario este escuchando
 interm_address = ('localhost', port_number)
 client_sock.connect(interm_address)
+client_sock.settimeout(2)
 
 def rdt_send(message_array):
+    print "Entro a enviar"
     global next_seq_num, count
-    # Envia solo la ventana 
-    while next_seq_num < base_seq_num + window_size:
-        packet = str(next_seq_num)+':'+message_array[next_seq_num]
+    # Envia solo la ventana   
+    if next_seq_num < base_seq_num + window_size:
+        packet = str(next_seq_num)+':'+message_array[count-1]
         client_sock.sendall(packet)
         next_seq_num = count % seq_range # Asigna el siguiente numero de secuencia
+        print "Envio: "+packet+" Next seq number: "+str(next_seq_num)+" Base: "+str(base_seq_num)
         count += 1
         time.sleep(0.1)
+    else:
+        print "No"
 
 def rdt_recv():
+    print "Entro a recibir"
+    global base_seq_num
+    try:
+        data = client_sock.recv(19)
+    except socket.timeout, e:
+        print "timeout"
+        data = ""
+    if data:
+        ack_message = int(data)
+        base_seq_num = ack_message + 1
+        print "Recibo: "+data+" Next seq number: "+str(next_seq_num)+" Base: "+str(base_seq_num)
 
 def open_file():
     with open(sys.argv[2],'r') as file_to_send:
@@ -44,11 +60,10 @@ try:
     next_seq_num = 0
     count = 1
 
-    # Enviando datos 
-    rdt_send(message_array)
-
-    data = client_sock.recv(1000)
-    print >>sys.stderr, 'Recibiendo %s' % data
+    while True:
+        # Enviando datos 
+        rdt_send(message_array)
+        rdt_recv()
 
 finally:
     print >>sys.stderr, 'Cerrando socket'
--- intermediario.py
+++ intermediario.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-
 # Programa Intermediario
+# Argumentos en la linea de comandos
+# python [0: intermediario.py] [1: modo] [2: puerto_interm] [3: puerto_servidor] [4: probabilidad]
+
 import socket
 import sys
+import time
  
 # Creando el socket TCP/IP para escuchar al cliente
 iclent_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Creando el socket TCP/IP para conectarse al servidor
 iservr_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-'''
-Argumentos en la linea de comandos
-python [0: intermediario.py] [1: modo] [2: puerto_interm] [3: puerto_servidor] [4: probabilidad]
-'''
 # Obtiene el numero de puerto donde escucha el intermediario
 interm_port = int(sys.argv[2])
 # Obtiene el numero de puerto donde se conecta con el servidor
@@ -22,7 +21,6 @@ server_port = int(sys.argv[3])
 interm_address = ('localhost', interm_port)
 # Enlace de socket y el puerto del servidor
 server_address = ('localhost', server_port)
-print >>sys.stderr, 'Empezando a levantar %s en el puerto %s' % interm_address
 iclent_sock.bind(interm_address)
 iservr_sock.connect(server_address)
 
@@ -31,23 +29,23 @@ iclent_sock.listen(1)
  
 while True:
     # Esperando conexion
-    print >>sys.stderr, 'Esperando para conectarse'
     connection, client_address = iclent_sock.accept()
  
     try:
-        print >>sys.stderr, 'Conexion desde', client_address
- 
         # Recibe los datos en trozos y reetransmite
         while True:
             data = connection.recv(1000)
             if data:
                 print >>sys.stderr, 'Recibido "%s"' % data
                 iservr_sock.sendall(data)
-                print >>sys.stderr, 'Enviando mensaje de vuelta al cliente'
-                connection.sendall(data)
             else:
                 print >>sys.stderr, 'No hay mas datos', client_address
                 break
+
+            data_from_server = iservr_sock.recv(100)
+            if data_from_server:
+                print >>sys.stderr, 'Enviando mensaje de vuelta al cliente "%s"' % data_from_server
+                connection.sendall(data_from_server)
              
     finally:
         # Cerrando conexion
